d91e82199187272200e8f6c74e019f47e8bde16f
kyoko <r00t.localhost@hotmail.com>
2017-02-17 12:22:23 +0800

    Added Lewdbot

---
+++ readme.md
@@ -0,0 +1,18 @@
+#Telegram Lewdbot
+
+###A Simple Bot That Scrapes Lewd Images on DanBooru
+Installation
++ Generate an API Key at http://danbooru.donmai.us/
++ Setup the Bot via Telegram Bot Master.
++ Paste The API Keys on the config.json file
+
+Commands
+* /start@bot_name - Starts The Lewd Bot
+* /getlewds - Gets Random Lewds of Your Waifu
+
+Usage
+* /getlewds kousaka honoka
+
+**Example**
+
+Add http://t.me/glph_henbot on your Channel
---
+++ web/hook.php
@@ -0,0 +1,55 @@
+<?php
+ini_set('error_reporting', E_ALL);
+
+$config = json_decode(
+    file_get_contents('config.json')
+);
+
+//Telegram Hook Api key
+$token = $config->telegram->bot_token;
+$site='https://api.telegram.org/bot'.$token;
+
+//Booru Api Key
+$apikey = $config->booru->api_key;
+$login = $config->booru->booru_username;
+
+$update = json_decode(file_get_contents('php://input'), true);
+
+$id = $update["message"]["chat"]["id"];
+$msg = $update["message"]["text"];
+
+$keyword = str_replace('/getlewds ','',$msg);
+$query = str_replace(' ','_',$keyword);
+
+$booruAPI = json_decode(
+    file_get_contents('https://danbooru.donmai.us/posts.json?limit=1&tags='.$query.'%20rating:e&random=true&login='.$login.'&api_key='.$apikey),
+    true);
+
+$urlkey= $booruAPI[0]['file_url'];
+$sauce = $booruAPI[0]['source'];
+
+switch ($msg){
+    case '/getlewds '.$keyword:
+        sendImage($id,'https://danbooru.donmai.us'.$urlkey);
+        if(empty($booruAPI)){
+            sendMessage($id,'No Lewds Found for '.$query);
+        }
+        break;
+
+    case '/start':
+        sendMessage($id,'Yahallo!');
+        break;
+}
+
+
+function sendMessage($id,$msg){
+    $url = $GLOBALS[site].'/sendMessage?chat_id='.$id.'&text='.urlencode($msg);
+    file_get_contents($url);
+}
+
+function sendImage($id,$photo){
+    $url = $GLOBALS[site].'/sendPhoto?chat_id='.$id.'&photo='.$photo;
+    file_get_contents($url);
+}
+
+?>
